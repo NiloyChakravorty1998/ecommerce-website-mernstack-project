4cf58ba57134a427b2ca5821b83044e71905b64e
Niloy Chakravorty <chakravortyniloy@gmail.com>
2023-08-04 01:25:35 +0530

    added bootstrap and navbar

--- ecommerce-client/src/App.jsx
+++ ecommerce-client/src/App.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
+import Header from './components/Header'
+import { Container } from 'react-bootstrap'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
-    <div>
-      <h1>
-      welcome
-      </h1>
-    </div>
+    <>
+    <Header/>
+    <main className='py-3'>
+    <Container>
+      <h1>Welcome</h1>
+    </Container>
+    </main>
+    <Footer/>
+    </>
   )
 }
 
