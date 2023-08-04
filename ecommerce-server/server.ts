6e469ca2c08a77ad30e89f4ac32aecfeb0da598f
Niloy Chakravorty <chakravortyniloy@gmail.com>
2023-08-05 02:23:11 +0530

    added dotenv

--- ecommerce-server/server.ts
+++ ecommerce-server/server.ts
@@ -1,12 +1,23 @@
 import express, { Request, Response } from "express";
+import products from './data/products.js'
+import dotenv from 'dotenv'
 
-const port : number = 5000;
+dotenv.config();
+const port : string = process.env.PORT!;
 const app = express();
 
 app.get('/', (req:Request,res:Response)=> {
     res.send('Hello!')
 });
 
+app.get('/api/products',(req: Request, res:Response) => {
+    res.status(200).json(products);
+})
+
+app.get('/api/products/:id',(req: Request, res:Response) => {
+    const product = products.find((p) => p._id === req.params.id);
+    res.status(200).json(product);
+})
 app.listen(port,() => {
     console.log(`App started on port: ${port}`);
 })
