01836edffc636d044f905087dab3e4d28c164736
Niloy Chakravorty <chakravortyniloy@gmail.com>
2023-08-04 17:46:25 +0530

    router added

--- ecommerce-client/src/main.jsx
+++ ecommerce-client/src/main.jsx
@@ -9,13 +9,13 @@ import HomeScreen from './screens/HomeScreen.jsx'
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<App/>}>
-    <Route path='/' element={<HomeScreen/>}></Route>
+    <Route index={true} path='/' element={<HomeScreen/>}></Route>
     </Route>
   )
 )
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <RouterProvider router={router}/>
+  </React.StrictMode>
 )
